d991a20e6384a71a45682ff8335b42f8957f58a2
Rami <rami@workpop.com>
2017-08-03 17:36:51 -0700

    add tests

--- src/influx.js
+++ src/influx.js
@@ -1,5 +1,5 @@
 // @flow
-import { get, mapKeys, reduce, map } from 'lodash';
+import { get, mapKeys, reduce } from 'lodash';
 
 export function formatMetricsForInfluxDBPoint(data: Object, measurement: string, tags: Object): Object {
   const timestamp = get(data, 'timestamp');
@@ -23,14 +23,27 @@ export function formatMetricsForInfluxDBPoint(data: Object, measurement: string,
   });
 }
 
-export function InfluxMetricsQueue(tags: Object, fields: Object, flushFunc: Function) {
-  this.points = [];
-  this.tags = tags || {};
-  this.fields = fields || {};
-  this.flushFunc = flushFunc || console.log;
+type InfluxMetricsQueueInputType = {
+  tags: Object,
+  fields: Object,
+  flushFunc: Function,
 }
 
-InfluxMetricsQueue.prototype.createOperation = function(operationName: string, tags: Object, fields: Object) {
+type PointType = {
+  timestamp: number,
+  tags: Object,
+  fields: Object,
+  measurement: string,
+};
+
+export function InfluxMetricsQueue({tags = {}, fields = {}, flushFunc = console.log}: InfluxMetricsQueueInputType) { // eslint-disable-line no-console
+  this.points = [];
+  this.tags = tags;
+  this.fields = fields;
+  this.flushFunc = flushFunc; // eslint-disable-line no-console
+}
+
+InfluxMetricsQueue.prototype.createOperation = function (operationName: string, tags: Object, fields: Object): Object {
   return new Operation(this, operationName, {
     ...this.tags,
     ...tags,
@@ -38,78 +51,83 @@ InfluxMetricsQueue.prototype.createOperation = function(operationName: string, t
     ...this.fields,
     ...fields,
   });
-}
+};
 
-InfluxMetricsQueue.prototype.getPoints = function() {
+InfluxMetricsQueue.prototype.getPoints = function (): Array<PointType> {
   return this.points;
-}
+};
 
-InfluxMetricsQueue.prototype.clearPoints = function() {
+InfluxMetricsQueue.prototype.clearPoints = function () {
   this.points = [];
-}
+};
 
-InfluxMetricsQueue.prototype.addPoint = function(point) {
+InfluxMetricsQueue.prototype.addPoint = function (point: PointType) {
   this.points.push(point);
-}
+};
 
-InfluxMetricsQueue.prototype.flush = function() {
-  const points = map(this.getPoints(), (point) => {
-
-  });
+InfluxMetricsQueue.prototype.flush = function (): any {
+  const points = this.getPoints();
   this.clearPoints();
-  const flushResult = this.flushFunc(points);
-}
-
+  return this.flushFunc(points);
+};
 
 //InfluxMetricsQueue instantiates instances of Operation for each operation that needs to create influx data
 //This instance is responsible for timing different operations and queueing data points into the influx queue
-function Operation(queue: Object, operationName: string) {
+function Operation(queue: Object, operationName: string, tags: Object = {}, fields: Object = {}) {
   this.queue = queue;
   this.operationName = operationName;
+  this.tags = tags;
+  this.fields = fields;
 }
 
 //This returns a new instance of of an operation timer
 //It starts timing as soon as this function is called
-Operation.prototype.startOperationTimer = function() {
+Operation.prototype.startTimer = function (): Object {
   return new OperationTimer(this);
-}
+};
 
 //This creates a new influx data point with the provided tags and fields
 //tags should identify and segment this operation
 //fields should be values that you want to measure, eg response timestamp
-Operation.prototype.queuePoint = function(tags: Object, fields: Object) {
+Operation.prototype.queuePoint = function (tags: Object, fields: Object) {
   const point = new Point({
     timestamp: Date.now(),
     tags: {
       ...this.tags,
       ...tags,
     },
-    fields,
+    fields: {
+      ...this.fields,
+      ...fields,
+    },
     measurement: this.operationName,
   });
-  this.queue.addPoint(point)
-}
+  this.queue.addPoint(point);
+};
 
-function OperationTimer(operation) {
+function OperationTimer(operation: Object) {
   this.operation = operation;
   this.startTime = process.hrtime();
 }
 
-OperationTimer.prototype.completed = function(tags, fields) {
+OperationTimer.prototype.completed = function (tags: Object = {}, fields: Object = {}) {
   const responseTimeMs = this.elapsedTime();
-  return this.operation.queuePoint(tags, fields);
-}
+  this.operation.queuePoint(tags, {
+    ...fields,
+    responseTimeMs,
+  });
+};
 
-OperationTimer.prototype.elapsedTime = function() {
+OperationTimer.prototype.elapsedTime = function (): number {
   const diff = process.hrtime(this.startTime);
   return (diff[0] * 1000) + Math.round(diff[1] / 1000000);
-}
+};
 
-function Point({ timestamp, fields, tags, measurement }: { timestamp: number, fields: Object, tags: Object, measurement: string}) {
+function Point({ timestamp, fields, tags, measurement }: { timestamp: number, fields: Object, tags: Object, measurement: string}): Object {
   return {
     timestamp,
     fields,
     tags,
     measurement,
-  }
+  };
 }
--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
-import { get } from 'lodash';
+import { get, forEach } from 'lodash';
 import {
   MetricCounter,
-  formatMetricsForInfluxDBPoint,
+  InfluxMetricsQueue,
 } from '../src';
 
 const expect = require('chai').expect;
@@ -27,5 +27,117 @@ describe('Test Counter', function () {
       expect(get(currentBucket, 'operations.brew.total_response_time')).to.equal(elapsedTime);
       done();
     }, 1000);
+  });
+});
+
+
+describe('InfluxMetricsQueue', function () {
+  function flushFunc(data) {
+    return data;
+  }
+  it('should time operations with dynamic fields/tags from the queue input paramters', function () {
+    const testQueue = new InfluxMetricsQueue({
+      fields: {
+        thisissparta: 300,
+      },
+      tags: {
+        pizza: 'taco',
+      },
+      flushFunc,
+    });
+
+    const operation = testQueue.createOperation('test');
+    const opTimer = operation.startTimer();
+    opTimer.completed();
+
+    const results = testQueue.flush();
+    expect(results).to.be.a('array');
+
+    results.forEach((point) => {
+      expect(point.fields).to.deep.include.keys('thisissparta');
+      expect(point.tags).to.deep.include.keys('pizza');
+      expect(point.fields.thisissparta).to.equal(300);
+      expect(point.tags.pizza).to.equal('taco');
+    });
+  });
+  it('should time operations with dynamic fields/tags from the operation input paramters', function () {
+    const testQueue = new InfluxMetricsQueue({
+      flushFunc,
+    });
+
+    const operation = testQueue.createOperation('test', {
+      pizza: 'taco',
+    }, {
+      thisissparta: 300,
+    });
+    const opTimer = operation.startTimer();
+    opTimer.completed();
+
+    const results = testQueue.flush();
+    expect(results).to.be.a('array');
+
+    results.forEach((point) => {
+      expect(point.fields).to.deep.include.keys('thisissparta');
+      expect(point.tags).to.deep.include.keys('pizza');
+      expect(point.fields.thisissparta).to.equal(300);
+      expect(point.tags.pizza).to.equal('taco');
+    });
+  });
+  it('should time operations with dynamic fields/tags from the timer input paramters', function () {
+    const testQueue = new InfluxMetricsQueue({
+      flushFunc,
+    });
+
+    const operation = testQueue.createOperation('test');
+    const opTimer = operation.startTimer();
+    opTimer.completed({
+      pizza: 'taco',
+    }, {
+      thisissparta: 300,
+    });
+
+    const results = testQueue.flush();
+    expect(results).to.be.a('array');
+
+    results.forEach((point) => {
+      expect(point.fields).to.deep.include.keys('thisissparta');
+      expect(point.tags).to.deep.include.keys('pizza');
+      expect(point.fields.thisissparta).to.equal(300);
+      expect(point.tags.pizza).to.equal('taco');
+    });
+  });
+  it('should return one unique point for each timed operation', function () {
+    const testQueue = new InfluxMetricsQueue({
+      flushFunc,
+    });
+
+    const operation = testQueue.createOperation('test');
+    const opTimer = operation.startTimer();
+    opTimer.completed({
+      pizza: 'taco',
+    }, {
+      thisissparta: 300,
+    });
+
+    const opTimer2 = operation.startTimer();
+    opTimer2.completed({
+      taco: 'pizza',
+    }, {
+      batman: 'robin',
+    });
+
+    const results = testQueue.flush();
+    expect(results).to.be.a('array');
+    expect(results.length).to.equal(2);
+    const point1 = results[0];
+    expect(point1.fields).to.deep.include.keys('thisissparta');
+    expect(point1.tags).to.deep.include.keys('pizza');
+    expect(point1.fields.thisissparta).to.equal(300);
+    expect(point1.tags.pizza).to.equal('taco');
+    const point2 = results[1];
+    expect(point2.fields).to.deep.include.keys('batman');
+    expect(point2.tags).to.deep.include.keys('taco');
+    expect(point2.fields.batman).to.equal('robin');
+    expect(point2.tags.taco).to.equal('pizza');
   });
 });
